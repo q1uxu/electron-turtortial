4093f25f86afb961f8a5ba917fa5c4de985aa04b
秦秋旭 <qiuxu.qin@outlook.com>
2022-11-11 11:00:09 +0800

    ✨ 检测更新程序代码

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+require('update-electron-app')()
 
 const createWindow = () => {
   const win = new BrowserWindow({
